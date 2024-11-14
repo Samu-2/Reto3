4c069a3ab477b1ab479b0535fb6c816455b6fcb6
Carlos809-stack <carloscalvobernal@gmail.com>
2024-11-14 09:29:16 +0100

    creado funciones2

--- Reto3/src/ejercicios/Funciones2.java
+++ Reto3/src/ejercicios/Funciones2.java
@@ -1,5 +1,24 @@
 package ejercicios;
 
-public class Funciones2 {
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class  Funciones2 {
+	public static double mediadeFechaDias(LocalDate fecha1, LocalDate fecha2) {
+		int meses = (int) ChronoUnit.MONTHS.between(fecha1, fecha2);
+		Math.abs(meses);
+		int cont = 0;
+		double dias = 0;
+		for(int i = 0; i<meses; i++) {
+			cont=cont+fecha1.lengthOfMonth();
+			fecha1= fecha1.plusMonths(1);
+			
+			
+		}
+		dias = cont / meses;
+		return dias;
+	}
+
+	
 
 }
--- Reto3/src/ejercicios/reto.java
+++ Reto3/src/ejercicios/reto.java
@@ -1,10 +1,14 @@
 package ejercicios;
 
+import java.time.LocalDate;
+
 public class reto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		LocalDate fecha1 = LocalDate.now();
+		LocalDate fecha2 = LocalDate.of(2025,11,14);
+		Funciones2.mediadeFechaDias(fecha1, fecha2);
+		
 	}
 
 }
