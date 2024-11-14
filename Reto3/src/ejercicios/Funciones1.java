26a2eb19568b2b05d39f8598125799fedb285ef7
Samu-2 <samuel.m27a@gmail.com>
2024-11-14 08:45:23 +0100

    difmeses

--- Reto3/src/ejercicios/Funciones1.java
+++ Reto3/src/ejercicios/Funciones1.java
@@ -1,5 +1,42 @@
 package ejercicios;
 
-public class Funciones1 {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
 
+public class Funciones1 {
+	public static LocalDate dimeFecha(String Texto, Scanner sc) {
+
+		DateTimeFormatter formato1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+		do {
+			try {
+
+				System.out.println(Texto);
+				String fechaTexto = sc.nextLine();
+				LocalDate fecha = LocalDate.parse(fechaTexto, formato1);
+		
+				return fecha;
+
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("Introduce una fecha coherente");
+			}
+
+		} while (true);
+	}
+	
+	public static int diferenciaMeses(LocalDate fecha1,LocalDate fecha2) {
+		int meses = (int) ChronoUnit.MONTHS.between(fecha1, fecha2);
+		Math.abs(meses);
+		return meses;
+
+	}
+	public static boolean fechaMasAntigua(LocalDate fecha1,LocalDate fecha2) {
+		if(fecha2.isAfter(fecha1))
+			return true;
+		else
+			return false;
+	}
 }
